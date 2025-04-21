b2c808811d5eef2113cde60fc1241c154b3cb8a2
khuram <muhamadkhuram1999@gmail.com>
2025-04-21 12:39:35 +0500

    Implement Time class with methods to convert to and from seconds; update App class to demonstrate functionality

--- Circle/src/App.java
+++ Circle/src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         Circle circle = new Circle(5);
         System.out.println("Area: " + circle.calculateArea());
